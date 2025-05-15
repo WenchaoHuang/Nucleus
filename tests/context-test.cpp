34fc272646477edfc344380a785dc57eca87bcce
Wen-Chao Huang <physhuangwenchao@gmail.com>
2025-05-15 23:24:34 +0800

    Add 'nucleus-tests' project

---
+++ tests/context-test.cpp
@@ -0,0 +1,40 @@
+﻿/**
+ *	Copyright (c) 2025 Wenchao Huang <physhuangwenchao@gmail.com>
+ *
+ *	Permission is hereby granted, free of charge, to any person obtaining a copy
+ *	of this software and associated documentation files (the "Software"), to deal
+ *	in the Software without restriction, including without limitation the rights
+ *	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *	copies of the Software, and to permit persons to whom the Software is
+ *	furnished to do so, subject to the following conditions:
+ *
+ *	The above copyright notice and this permission notice shall be included in all
+ *	copies or substantial portions of the Software.
+ *
+ *	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *	SOFTWARE.
+ */
+
+#include <nucleus/context.hpp>
+
+/*************************************************************************
+***************************    context_test    ***************************
+*************************************************************************/
+
+void context_test()
+{
+	NS_ASSERT(ns::Version(10, 2) < ns::Version(10, 3));
+	NS_ASSERT(ns::Version(10, 4) > ns::Version(10, 3));
+	NS_ASSERT(ns::Version(10, 5) == ns::Version(10, 5));
+
+	auto context = ns::Context::getInstance();
+	auto driverVersion = context->getDriverVersion();
+	auto runtimVersion = context->getRuntimeVersion();
+	auto devices = context->getDevices();
+	auto device = context->getDevice(0);
+}
